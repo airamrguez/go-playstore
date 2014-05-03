c8b0325032ac006fb52264f2612c23b5c024104a
airamrguez <airamrguezrivero@gmail.com>
2014-05-03 20:57:19 +0200

    some more tests

--- app_test.go
+++ app_test.go
@@ -17,6 +17,28 @@ func TestTitle(t *testing.T) {
 	}
 }
 
+func TestCategory(t *testing.T) {
+	app, err := LookUp(httpGet, "com.google.android.youtube")
+	if err != nil {
+		t.Logf("Error: %s.", err.Error())
+	} else {
+		if app.Category != "Media & Video" {
+			t.Error("Expected app category to be the same.")
+		}
+	}
+}
+
+func TestDeveloperName(t *testing.T) {
+	app, err := LookUp(httpGet, "com.google.android.youtube")
+	if err != nil {
+		t.Logf("Error: %s.", err.Error())
+	} else {
+		if app.Developer.Name != "Google Inc." {
+			t.Error("Expected app category to be the same.")
+		}
+	}
+}
+
 func httpGet(url *url.URL) (*http.Response, error) {
 	return http.Get(url.String())
 }
